706ae5c23fc9fd88b1c1dfbc8f93c0482f3f4698
noisenuisance <sabauspne@gmail.com>
2023-03-24 23:24:44 +0300

    feat: add AddButton component

---
+++ src/components/AddButton.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <button class="add-button">+</button>
+</template>
+
+<style lang="scss">
+.add-button {
+  width: 225px;
+  height: 120px;
+  background-color: $bg-grey;
+  color: $primary-dark;
+  border: none;
+  outline: none;
+}
+</style>
