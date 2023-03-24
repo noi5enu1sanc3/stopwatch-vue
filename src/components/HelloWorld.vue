8af98604c5fb9bcb57be5ddf43a75ff58a7af071
noisenuisance <sabauspne@gmail.com>
2023-03-24 20:38:04 +0300

    chore: add normalize and import scss variables globally

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,8 +33,8 @@ const count = ref(0);
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
+<style lang="scss">
 .read-the-docs {
-  color: #888;
+  color: $bg-dark-grey;
 }
 </style>
