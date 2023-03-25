6ecb788931b6921cff5a5461533cf3860966473e
noisenuisance <sabauspne@gmail.com>
2023-03-25 19:26:11 +0300

    style: add AddButton icon and hover effects

--- src/components/ui/AddButton.vue
+++ src/components/ui/AddButton.vue
@@ -1,7 +1,9 @@
 <script></script>
 
 <template>
-  <button class="add-button">+</button>
+  <button class="add-button">
+    <div class="add-button__icon" />
+  </button>
 </template>
 
 <style lang="scss">
@@ -12,5 +14,21 @@
   color: $primary-dark;
   border: none;
   outline: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__icon {
+    width: 20px;
+    -webkit-mask-image: url('../../assets/icons/cross-icon.svg');
+    mask-image: url('../../assets/icons/cross-icon.svg');
+    height: 20px;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-color: $primary-dark;
+    transition: background-color 0.3s ease-in;
+  }
+  &:hover &__icon {
+    background-color: $primary;
+  }
 }
 </style>
