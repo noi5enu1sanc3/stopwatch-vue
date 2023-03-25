832ac8bc569a128471733f842bda613b96f17aee
noisenuisance <sabauspne@gmail.com>
2023-03-25 17:48:09 +0300

    style: add start/pause button animation

--- src/components/StopWatch.vue
+++ src/components/StopWatch.vue
@@ -1,17 +1,43 @@
 <script setup>
-import { ref } from 'vue';
+import { useStopwatch } from '../composables/useStopwatch';
+import * as format from '../utils/formatTime';
+const { hours, minutes, seconds, isRunning, handleToggleRunning, handleReset } =
+  useStopwatch();
 </script>
 
 <template>
-  <div class="stopwatch">
+  <div class="stopwatch" :class="{ stopwatch_running: isRunning }">
     <div class="stopwatch__display">
-      <span class="stopwatch__value stopwatch__value_hours"></span>
-      <span class="stopwatch__value stopwatch__value_minutes"></span>
-      <span class="stopwatch__value stopwatch__value_seconds"></span>
+      <span class="stopwatch__value">{{
+        format.getString(hours, minutes, seconds)
+      }}</span>
+      <!-- <span v-if="hours > 0" class="stopwatch__value stopwatch__value_hours"
+        >{{ hours }} :
+      </span>
+      <span v-if="minutes > 0" class="stopwatch__value stopwatch__value_minutes"
+        >{{ minutes }} :
+      </span>
+      <span class="stopwatch__value stopwatch__value_seconds">{{
+        seconds
+      }}</span> -->
     </div>
     <div class="stopwatch__controls">
-      <button class="stopwatch__button stopwatch__button_start" />
-      <button class="stopwatch__button stopwatch__button_stop" />
+      <button
+        class="stopwatch__button stopwatch__button_type_start"
+        :class="{ stopwatch__button_type_pause: isRunning }"
+        @click="handleToggleRunning"
+      >
+        <span
+          class="stopwatch__button-element stopwatch__button-element-before"
+        />
+        <span
+          class="stopwatch__button-element stopwatch__button-element-after"
+        />
+      </button>
+      <button
+        class="stopwatch__button stopwatch__button_type_stop"
+        @click="handleReset"
+      />
     </div>
   </div>
 </template>
@@ -27,9 +53,13 @@ import { ref } from 'vue';
     align-items: center;
     border-bottom: 1px solid $primary-dark;
     height: 50%;
+    transition: all 0.3s ease-in;
   }
   &__value {
     color: $primary-dark;
+    font-size: $fs-default;
+    line-height: $lh-default;
+    transition: all 0.3s ease-in;
   }
   &__controls {
     height: 50%;
@@ -39,11 +69,49 @@ import { ref } from 'vue';
     align-items: center;
   }
   &__button {
-    background-color: $primary-dark;
+    background-color: transparent;
+    display: inline-block;
     width: 20px;
     height: 20px;
     border: none;
     outline: none;
+    position: relative;
+    &-element {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background-color: $primary-dark;
+      position: absolute;
+      top: 0;
+      left: 0;
+      transition: clip-path 0.3s ease, background-color 0.3s ease;
+    }
+  }
+  &__button_type_start &__button-element-before {
+    clip-path: polygon(0 0, 50% 25%, 50% 75%, 0% 100%);
+  }
+  &__button_type_start &__button-element-after {
+    clip-path: polygon(50% 25%, 100% 50%, 100% 50%, 50% 75%);
+  }
+  &__button_type_pause &__button-element-before {
+    clip-path: polygon(0 0, 30% 0, 30% 100%, 0% 100%);
+  }
+  &__button_type_pause &__button-element-after {
+    clip-path: polygon(70% 0, 100% 0, 100% 100%, 70% 100%);
+  }
+  &__button_type_stop {
+    background-color: $primary-dark;
+    transition: background-color 0.3s ease;
+  }
+  &_running * {
+    color: $primary;
+    border-color: $primary;
+  }
+  &_running &__button_type_stop {
+    background-color: $primary;
+  }
+  &_running &__button-element {
+    background-color: $primary;
   }
 }
 </style>
