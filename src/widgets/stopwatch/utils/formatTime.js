6d55c8e8ed373cbc3e63019c5e5b290576db47e5
noisenuisance <sabauspne@gmail.com>
2023-03-27 13:38:29 +0300

    fix: formatting

--- src/widgets/stopwatch/utils/formatTime.js
+++ src/widgets/stopwatch/utils/formatTime.js
@@ -1,5 +1,7 @@
 const getString = (hrs, mins, secs) => {
-  return `${hrs > 0 ? hrs + ':' : ''}${mins > 0 ? mins + ':' : ''}${secs}`;
+  return `${hrs > 0 ? hrs + ':' : ''}${
+    mins > 0 || hrs > 0 ? mins + ':' : ''
+  }${secs}`;
 };
 
 export { getString };
